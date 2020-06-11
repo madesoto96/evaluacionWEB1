2dfc2fe42c5f124e23a000ab354bd061981d52c0
madesoto96 <62155698+madesoto96@users.noreply.github.com>
2020-06-11 16:58:18 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<? php 
+
+//primer ejercicio
+
+/*$numero1=10;
+$numero2=15;
+
+$suma=$numero1 + $numero2
+$resta=$numero1 - $numero2
+$multiplicacion=$numero1 * $numero2
+$division=$numero1 / $numero2
+
+echo("CALCULADORA 2020");
+echo("<br>");
+echo("..................");
+echo("<br>");
+echo("<br>");
+
+echo("la suma de: " . $numero1. "+" . $numero2 "=" . $suma);
+echo("<br>");
+echo("la resta de: " . $numero1. "-" . $numero2 "=" . $resta);
+echo("<br>");
+echo("la mutiplicacion de: " . $numero1. "* " . $numero2 "=" . $multiplicacion);
+echo("<br>");
+echo("la division de: " . $numero1. "/" . $numero2 "=" . $division);*/
+
+
+
+
+// segundo ejercicio
+
+// Almacenar el IMC de cada persona
+//gimnasio bodytech//
+/*$peso = 85;
+$altura = 1.78;
+$imc = peso/(altura*altura);
+
+//condicionales//
+if($imc <= 18.4){
+    echo("tu imc es de ".$imc." Tienes insuficiencia de peso");
+
+} else{
+    if(18.5 <= $imc && <= 24.9){
+       echo("tu imc es de ".$imc." Tienes un peso normal");
+    }
+} else {
+    if(25 <= $imc && <= 29.9){
+       echo("tu imc es de ".$imc." tienes sobrepeso");
+    }
+} else{
+    if (30 <= $imc && <= 34.9){
+        echo("tu imc es de ".$imc." Tienes obesidad 1");
+
+    }
+} else{
+    if(35 <= $imc && <= 39.9){
+        echo("tu imc es de ".$imc." tienes obesidad 2");
+    }
+} else{
+    if($imc <= 40){
+        echo("tu imc es de ".$imc." tienes obesidad 3");
+    }
+
+}*/
+
+
+//tercer ejercicio
+
+/*$ cantidadZapatos = 5;
+$ costoneto = 150000
+$ totalCompra = ($ cantidadZapatos * costoneto);
+
+$ valorPagar = ($ cantidadZapatos * $ costoneto) - $ valordescuento;
+
+
+if ($ cantidadZapatos == 3) {
+  $ valordescuento = $ totalCompra * 0,10;
+    
+} else if ($ cantidadZapatos > 4 && $ cantidadZapatos < = 5) {
+  $ valordescuento = $ totalCompra * 0,20;
+    
+} else if ($ cantidadZapatos > 5) {
+  $ valordescuento = $ totalCompra * 0,40;
+}
+console.log ( "Cantidad Vendida:".. $ cantidadZapatos " < br > Coste Neto:" $ totalCompra..
+" < Br > Valor del Descuento:". $ valordescuento. " < Br > Valor de un Pagar es:" + $ valorPagar); */
+
+
+?>
