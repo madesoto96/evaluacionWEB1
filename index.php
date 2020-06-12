65b3f576f254c27ac5451a89c2b75828d6568f5c
madesoto96 <62155698+madesoto96@users.noreply.github.com>
2020-06-12 11:39:17 -0500

    solucion de los 5 ejercicios taller 1

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
-<? php 
+<?php
+    
 
 //primer ejercicio
 
 /*$numero1=10;
 $numero2=15;
 
-$suma=$numero1 + $numero2
-$resta=$numero1 - $numero2
-$multiplicacion=$numero1 * $numero2
-$division=$numero1 / $numero2
+$suma=$numero1 + $numero2;
+$resta=$numero1 - $numero2;
+$multiplicacion=$numero1 * $numero2;
+$division=$numero1 / $numero2;
 
 echo("CALCULADORA 2020");
 echo("<br>");
@@ -16,13 +17,13 @@ echo("..................");
 echo("<br>");
 echo("<br>");
 
-echo("la suma de: " . $numero1. "+" . $numero2 "=" . $suma);
+echo("la suma de: " . $numero1. "+" . $numero2. "=" . $suma);
 echo("<br>");
-echo("la resta de: " . $numero1. "-" . $numero2 "=" . $resta);
+echo("la resta de: " . $numero1. "-" . $numero2. "=" . $resta);
 echo("<br>");
-echo("la mutiplicacion de: " . $numero1. "* " . $numero2 "=" . $multiplicacion);
+echo("la mutiplicacion de: " . $numero1. "* " . $numero2. "=" . $multiplicacion);
 echo("<br>");
-echo("la division de: " . $numero1. "/" . $numero2 "=" . $division);*/
+echo("la division de: " . $numero1. "/" . $numero2. "=" . $division);*/
 
 
 
@@ -33,57 +34,130 @@ echo("la division de: " . $numero1. "/" . $numero2 "=" . $division);*/
 //gimnasio bodytech//
 /*$peso = 85;
 $altura = 1.78;
-$imc = peso/(altura*altura);
+$imc = $peso/($altura*$altura);
 
 //condicionales//
 if($imc <= 18.4){
     echo("tu imc es de ".$imc." Tienes insuficiencia de peso");
+  
 
-} else{
-    if(18.5 <= $imc && <= 24.9){
+} if(18.5 <= $imc && $imc <= 24.9){
        echo("tu imc es de ".$imc." Tienes un peso normal");
+        
     }
-} else {
-    if(25 <= $imc && <= 29.9){
+ if(25 <= $imc && $imc <= 29.9){
        echo("tu imc es de ".$imc." tienes sobrepeso");
-    }
-} else{
-    if (30 <= $imc && <= 34.9){
+     }  
+ if (30 <= $imc && $imc <= 34.9){
         echo("tu imc es de ".$imc." Tienes obesidad 1");
+       
 
     }
-} else{
-    if(35 <= $imc && <= 39.9){
+ 
+if(35 <= $imc && $imc <= 39.9){
         echo("tu imc es de ".$imc." tienes obesidad 2");
-    }
-} else{
-    if($imc <= 40){
+        
+  }
+if(40 <= $imc ){
         echo("tu imc es de ".$imc." tienes obesidad 3");
-    }
+    } */     
 
-}*/
+
 
 
 //tercer ejercicio
 
-/*$ cantidadZapatos = 5;
-$ costoneto = 150000
-$ totalCompra = ($ cantidadZapatos * costoneto);
-
-$ valorPagar = ($ cantidadZapatos * $ costoneto) - $ valordescuento;
+/*$cantidadZapatos = 5;
+$costoneto = 150000;
+$totalCompra = ($cantidadZapatos * $costoneto);
 
 
-if ($ cantidadZapatos == 3) {
-  $ valordescuento = $ totalCompra * 0,10;
+if ($cantidadZapatos == 3) {
+    $valordescuento = 10;
+ $valorPagar = $totalCompra*10/100;
     
-} else if ($ cantidadZapatos > 4 && $ cantidadZapatos < = 5) {
-  $ valordescuento = $ totalCompra * 0,20;
+} else if ($cantidadZapatos > 4 && $cantidadZapatos <= 5) {
+    $valordescuento = 20;
+  $valorPagar = $totalCompra *20/100;
     
-} else if ($ cantidadZapatos > 5) {
-  $ valordescuento = $ totalCompra * 0,40;
+} else if ($cantidadZapatos > 5) {
+    $valorPagar = 40;
+  $valorPagar = $totalCompra * 40/100;
 }
-console.log ( "Cantidad Vendida:".. $ cantidadZapatos " < br > Coste Neto:" $ totalCompra..
-" < Br > Valor del Descuento:". $ valordescuento. " < Br > Valor de un Pagar es:" + $ valorPagar); */
+echo ( "Cantidad Vendida:". $cantidadZapatos. " < br > Coste Neto:" .$totalCompra.
+" < Br > Valor del Descuento:". $valordescuento. " < Br > Valor de un Pagar es:" .$valorPagar);*/
+ 
+
+
+//cuarto ejercicio// 
+   /* $hora = 36;
+   
+    
+    if ($hora <= 40){
+        $valorHora = 20000;
+        $sueldo =20000*$hora;
+        echo("su sueldo semanal sera de " .$sueldo);
+    } else if($hora > 40){
+        $valorHora = 25000;
+        $sueldo = 25000*$hora;
+        echo("su sueldo semanal sera de " .$sueldo);
+    }*/
+  
+    
+
+//quinto ejercicio//
+
+//nombres//
+/*$nom1 = "Maria jose";
+$nom2 = "Catalina martinez";
+$nom3 = "martha gomez";
+$nom4 = "Santiago muñoz";
+$nom5 = "alex torres";
+    
+//telefono//
+$tel1 = "5780345";
+$tel2 = "5423890";
+$tel3 = "3116578";
+$tel4 = "3603339";
+$tel5 = "5093558";
+    
+//direcciones
+$dir1 = "cl 57a #90-60";
+$dir2 = "cr 47b #67-39";
+$dir3 = "cl 34a #24-69";
+$dir4 = "cr 78a #12-76";
+$dir5 = "cl 56b #33-14";
+    
+//salarios//
+$sal1 = 1000000;
+$sal2 = 800000;
+$sal3 = 750000;
+$sal4 = 900000;
+$sal5 = 1200000;
+
+//sumar//
+$sumatoriaSalarios=$sal1+$sal2+$sal3+$sal4+$sal5;
+$sucurb=20000000;
+$sucursc=15000000;
+ $mayorSalario;
+//imprimir//
+echo("<br>" .$nom1. $tel1. $dir1. $sal1);
+echo("<br>".$nom2. $tel2. $dir2. $sal2);
+echo("<br>" .$nom3. $tel3. $dir3. $sal3);
+echo("<br>" .$nom4. $tel4. $dir4. $sal4);
+echo("<br>" .$nom5. $tel5. $dir5. $sal5);
+
+if($sumatoriaSalarios >$sucurb && $sumatoriaSalarios>$sucursc){
+    $mayorSalario = $sumatoriaSalarios;
+    echo("el mayor salario es de la surcursal A".$sumatoriaSalarios);
+        
+    }else if($sucurb>$sumatoriaSalarios && $sucurb>$sucursc ){
+    $mayorSalario = $sucurb;
+    echo("el mayor salario es de la sucursal B".$sucurb);
+}else if($sucursc>$sumatoriaSalarios && $sucursc){
+    $mayorSalario = $sucursc;
+    echo("el mayor salario es de la sucursal C".$sucursc);
+}*/
 
 
 ?>
